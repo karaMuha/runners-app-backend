eb63b5bdc438c0ea50b481195c69cdc51adb6110
Muhammed Kara <n.muhammed.kara@gmail.com>
2024-05-01 16:07:52 +0200

    make testcontainer work

--- controllers/runnersController_test.go
+++ controllers/runnersController_test.go
@@ -1,17 +1,26 @@
 package controllers
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"path/filepath"
 	"runners/models"
 	"runners/repositories"
 	"runners/services"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	_ "github.com/lib/pq"
 )
 
 func initTestRouter(dbHandler *sql.DB) *http.ServeMux {
@@ -20,63 +29,69 @@ func initTestRouter(dbHandler *sql.DB) *http.ServeMux {
 	runnersService := services.NewRunnersService(runnersRepository, nil)
 	usersService := services.NewUsersService(usersRepository)
 	runnersController := NewRunnersController(runnersService, usersService)
+	usersController := NewUsersController(usersService)
 
 	router := http.NewServeMux()
 	router.HandleFunc("GET /runner", runnersController.GetRunnersBatch)
+	router.HandleFunc("POST /login", usersController.Login)
 
 	return router
 }
 
 func TestGetRunnersResponse(t *testing.T) {
-	dbHandler, mock, _ := sqlmock.New()
-	defer dbHandler.Close()
+	ctx := context.Background()
 
-	columns := []string{
-		"id",
-		"first_name",
-		"last_name",
-		"age",
-		"is_active",
-		"country",
-		"personal_best",
-		"season_best",
+	pgContainer, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("postgres:16.2-alpine"),
+		postgres.WithInitScripts(filepath.Join("..", "testdata", "init-db.sql")),
+		postgres.WithDatabase("runners-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("localtest"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").WithOccurrence(2).WithStartupTimeout(5*time.Second),
+		),
+	)
+
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	columnsUsers := []string{"user_role"}
+	t.Cleanup(func() {
+		err := pgContainer.Terminate(ctx)
+		if err != nil {
+			t.Fatalf("Failed to terminate pgContainer: %s", err)
+		}
+	})
 
-	mock.ExpectQuery("SELECT user_role").WillReturnRows(
-		sqlmock.NewRows(columnsUsers).AddRow(
-			"user",
-		),
-	)
+	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	require.NoError(t, err)
 
-	mock.ExpectQuery("SELECT *").WillReturnRows(
-		sqlmock.NewRows(columns).AddRow(
-			"5bbff343-29cf-43b5-acc6-6792e3f8074b",
-			"Adam",
-			"Smith",
-			30,
-			true,
-			"United States",
-			"02:00:41",
-			"02:13:13",
-		).AddRow(
-			"6153ccd1-4f64-4d92-8af7-2df6bbdfea66",
-			"Sarah",
-			"Smith",
-			30,
-			true,
-			"United States",
-			"01:18:28",
-			"01:18:28",
-		),
-	)
+	dbHandler, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		t.Fatalf("Failed to connect to database %s", err)
+	}
+
+	err = dbHandler.Ping()
+
+	if err != nil {
+		t.Fatalf("Failed to ping database: %v", err)
+	}
 
 	router := initTestRouter(dbHandler)
+	loginRequest, _ := http.NewRequest("POST", "/login", nil)
+	loginRequest.SetBasicAuth("user", "user")
+	loginRecorder := httptest.NewRecorder()
+	router.ServeHTTP(loginRecorder, loginRequest)
+
+	assert.Equal(t, http.StatusOK, loginRecorder.Result().StatusCode)
+
+	token := loginRecorder.Header().Get("Token")
+
 	request, _ := http.NewRequest("GET", "/runner", nil)
 	recorder := httptest.NewRecorder()
 
-	request.Header.Set("Token", "token")
+	request.Header.Set("Token", token)
 	router.ServeHTTP(recorder, request)
 
 	assert.Equal(t, http.StatusOK, recorder.Result().StatusCode)
@@ -85,7 +100,7 @@ func TestGetRunnersResponse(t *testing.T) {
 	json.Unmarshal(recorder.Body.Bytes(), &runners)
 
 	assert.NotEmpty(t, runners)
-	assert.Equal(t, 2, len(runners))
+	assert.Equal(t, 4, len(runners))
 }
 
 func TestGetRunnersErrResponseCountryAndYear(t *testing.T) {
